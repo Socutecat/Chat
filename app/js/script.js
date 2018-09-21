44097685b53e242f50c760bcc1ce487e09fa580b
Socutecat <Socutecat@github.com>
2018-09-21 22:19:56 +0300

    add:js script, dark theme, box-shadow on input and form

--- app/js/script.js
+++ app/js/script.js
@@ -1,21 +1,35 @@
+
 'use strict';
 //Init function toggleClass
 function toggleClass(clickTarget, classTarget, className){
-	var clickTarget = document.querySelector(clickTarget),
-		classTarget = document.querySelector(classTarget);
+	var clickTargetVariable = document.querySelector(clickTarget),
+		classTargetVariable = document.querySelector(classTarget);
 
-		document.onclick = function(e){
-			if((e.target !== classTarget) && (e.target !== clickTarget)){
-				classTarget.classList.remove(className);
-			};
-		};
+	function clickListener(){
+		event.stopPropagation();
+		this.classList.toggle('toggle-active');
+		classTargetVariable.classList.toggle(className);
+	}
 
-		clickTarget.onclick = function(){
-			event.preventDefault();
-			classTarget.classList.toggle(className);
-		};
-};;
+	clickTargetVariable.addEventListener('click', clickListener);
+
+	document.addEventListener('click', function(e){
+
+		if (e.target.closest(classTarget)){
+			return;
+		}
+
+		if (classTargetVariable.classList.contains(className)){
+			classTargetVariable.classList.remove(className);
+			clickTargetVariable.classList.remove('toggle-active');
+		}
+	});
+}
 
 // Using function toggleClass
+// Show and hide a list of users
 toggleClass('.toggle-button', '.sidebar', 'sidebar-open');
-
+// Drop-down menu
+toggleClass('.acc-status', '.acc-menu', 'acc-menu-open');
+// Theme switcher
+toggleClass('.toggle-box', 'body', 'night-mode');
